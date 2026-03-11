c4ea2abc5b96901c7995b3e81ed76593ad64ab58
Khang Nguyen <z5681219@ad.unsw.edu.au>
2026-03-11 23:27:40 +1100

    Integrated mathsoc-puzzle into React structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MathsocPuzzle from './mathsoc-puzzle'; // Import your component
+
+function App() {
+  return (
+    <div className="App">
+      <MathsocPuzzle />
+    </div>
+  );
+}
+
+export default App;
